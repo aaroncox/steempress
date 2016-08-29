17778eed9c15c5c43132b2f30bdc53dd361497d0
Aaron Cox <acox@carbinestudios.com>
2016-08-29 16:38:13 -0700

    Fixes from dudutaulois for the bootstrap theme

--- web/themes/bootstrap3-default/js/steempress-blog.js
+++ web/themes/bootstrap3-default/js/steempress-blog.js
@@ -38,3 +38,35 @@ jQuery(document).ready(function($) {
             });
     }
 });
+
+
+//ANIMATION
+
+
+
+    var $window = $(window);
+
+    function check_if_in_view() {
+      var $animation_elements = $('.animation-element');
+      var window_height = $window.height();
+      var window_top_position = $window.scrollTop();
+      var window_bottom_position = (window_top_position + window_height);
+     
+      $.each($animation_elements, function(index) {
+        var $element = $(this);
+        var element_height = $element.outerHeight();
+        var element_top_position = $element.offset().top;
+        var element_bottom_position = (element_top_position + element_height);
+     
+        //check to see if this current container is within viewport
+        if ((element_bottom_position >= window_top_position) &&
+            (element_top_position <= window_bottom_position)) {
+          $element.addClass('in-view');
+        } else {
+          $element.removeClass('in-view');
+        }
+      });
+}
+
+$window.on('scroll resize', check_if_in_view);
+$window.trigger('scroll');
